a45c49f90f7e938390c52240c8f580cc7f14d6e8
eliseekn <eliseekn@gmail.com>
2022-08-12 16:29:28 +0000

    Add download resume on click button action Update package author

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -9,7 +9,7 @@
                     <option value="fr">Fr</option>
                 </select>
 
-                <button type="button" class="btn">
+                <button type="button" class="btn" @click="handleDownloadResume">
                     Download
                 </button>
             </span>
@@ -20,7 +20,38 @@
 <script setup lang="ts">
     import {ref} from 'vue'
     import {languageStore} from "../../services/store"
+    import jsPDF from "jspdf"
+    import html2canvas from "html2canvas"
 
     const state = ref<string>(languageStore.state)
     const handleSetLanguage = () => {languageStore.set(state.value)}
+
+    //https://www.freakyjolly.com/multipage-canvas-pdf-using-jspdf/
+    const handleDownloadResume = async () => {
+        const htmlContainer = document.querySelector("#resume-content") as HTMLElement
+        const htmlHeight = htmlContainer?.clientHeight
+        const htmlWidth = htmlContainer?.clientWidth
+
+        const pdfWidth = htmlWidth
+        const pdfHeight = (pdfWidth * 1.5)
+        const totalPDFPages = Math.ceil(pdfHeight/pdfWidth) - 1
+
+        html2canvas(htmlContainer,{allowTaint:true})
+            .then(canvas => {
+                canvas.getContext('2d')
+
+                const imgData = canvas.toDataURL("image/jpeg", 1.0)
+                const pdf = new jsPDF('p', 'pt',  [pdfWidth, pdfHeight])
+
+                pdf.addImage(imgData, 'JPG', 0, 0, htmlWidth, htmlHeight)
+
+                for (let i = 1; i <= totalPDFPages; i++) {
+                    pdf.addPage([pdfWidth, pdfHeight])
+                    pdf.addImage(imgData, 'JPG', 0, - (pdfHeight * i), htmlWidth, htmlHeight)
+                }
+
+                pdf.save("resume.pdf")
+            })
+    }
+
 </script>
